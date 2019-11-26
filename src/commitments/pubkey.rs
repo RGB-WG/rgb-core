036fcf8fc54dbc066fbd9f294ee308b036a71f39
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2019-11-26 21:51:14 +0100

    Slight improvements

--- src/commitments/pubkey.rs
+++ src/commitments/pubkey.rs
@@ -18,7 +18,7 @@
 //! their wrapped bitcoin counterparts `bitcoin::PublickKey` and `bitcoin::PrivateKey`.
 
 use std::ops::{Index, RangeFull};
-use std::sync::{Once, ONCE_INIT};
+use std::sync::Once;
 use std::convert::TryInto;
 
 use secp256k1::{PublicKey, Secp256k1, All};
@@ -29,7 +29,7 @@ use crate::common::raw_representable::RawRepresentable;
 use crate::commitments::tag::BitcoinTag;
 
 const TAG: &'static str = "LNPBP-1";
-static INIT: Once = ONCE_INIT;
+static INIT: Once = Once::new();
 static mut PREFIX: [u8; 32] = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -44,15 +44,18 @@ impl Container for PubkeyCommitment {
     fn commit(&mut self, msg: &Self::Message) {
         let ec: Secp256k1<All> = Secp256k1::new();
         INIT.call_once(|| unsafe {
-            PREFIX = BitcoinTag::tag(TAG)[..].try_into().unwrap();
+            PREFIX = BitcoinTag::tag(TAG)[..].try_into()
+                .expect("SHA256 length is 32 bytes always");
         });
         unsafe {
-            let prefix = BitcoinTag::from_slice(&PREFIX).unwrap();
+            let prefix = BitcoinTag::from_slice(&PREFIX)
+                .expect("Can't fail since it reads hash value");
         }
         let origin = self.clone();
         let mut hmac_engine = HmacEngine::<sha256::Hash>::new(&origin.0.serialize());
         hmac_engine.input(&msg[..]);
         let factor = &Hmac::from_engine(hmac_engine)[..];
-        self.0.add_exp_assign(&ec, factor).expect("Must not fail");
+        self.0.add_exp_assign(&ec, factor)
+            .expect("Key tweaking has resulted in point at infinity");
     }
 }
