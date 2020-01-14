51d3914f6f6465eb8bcdfd5537f54f5eac2f8224
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-01-14 19:13:52 +0100

    Adding support for BTreeMap conmmitment serialization

--- src/csv/serialize/commitment/collections.rs
+++ src/csv/serialize/commitment/collections.rs
@@ -12,14 +12,22 @@
 // If not, see <https://opensource.org/licenses/MIT>.
 
 
+//! File implements commitment serialization according to LNPBP-5 standard for common Rust data
+//! structures: optionals `Option<T>`, vectors `Vec<T>` and ordered list in form of hash maps
+//! `HashMap<usize, T>.
+
+
 use std::io;
 use std::collections::{HashMap, BTreeMap};
 use super::{Commitment, Error};
 
 
+/// In terms of commitment serialization, we interpret `Option` as a zero-length `Vec`
+/// (for `Optional::None`) or single-item `Vec` (for `Optional::Some`). For deserialization
+/// an attempt to read `Option` from a serialized non-0 or non-1 length Vec will result in
+/// `Error::WrongOptionalEncoding`.
 impl<T> Commitment for Option<T> where T: Commitment {
     fn commitment_serialize<E: io::Write>(&self, mut e: E) -> Result<usize, Error> {
-        // We interpret optionals as zero- or single-value arrays
         Ok(match self {
             None => commitment_serialize_list!(e; 0usize),
             Some(val) => commitment_serialize_list!(e; 1usize, val),
@@ -27,7 +35,6 @@ impl<T> Commitment for Option<T> where T: Commitment {
     }
 
     fn commitment_deserialize<D: io::Read>(mut d: D) -> Result<Self, Error> {
-        // We interpret optionals as zero- or single-value arrays
         let mut vec = Vec::<T>::commitment_deserialize(&mut d)?;
         match vec.len() {
             0 => Ok(None),
@@ -39,6 +46,14 @@ impl<T> Commitment for Option<T> where T: Commitment {
     }
 }
 
+
+/// In terms of commitment serialization, `Vec` is stored in form of usize-serialized length
+/// (see `Commitemtn` implementation for `usize` type for serialization platform-independent
+/// constant-length serialization rules) followed by a consequently-serialized vec items,
+/// according to their type.
+///
+/// An attempt to serialize `Vec` with more items than can fit in `usize` serialization rules
+/// will result in `Error::OversizedVectorAllocation`.
 impl<T> Commitment for Vec<T> where T: Commitment {
     fn commitment_serialize<E: io::Write>(&self, mut e: E) -> Result<usize, Error> {
         let len = self.len() as usize;
@@ -60,20 +75,52 @@ impl<T> Commitment for Vec<T> where T: Commitment {
     }
 }
 
-impl<T> Commitment for HashMap<usize, T> where T: Commitment {
+
+/// LNP/BP library uses `HashMap<usize, T: Commitment>`s to serialize ordered lists, where the
+/// position of the list item must be fixed, since the item is referenced from elsewhere by its
+/// index. Thus, the library does not supports and recommends not to support commitment
+/// serialization of any other `HashMap` variants.
+///
+/// Commitment serialization of the `HashMap<usize, T>` type is performed by converting into
+/// a fixed-order `Vec<T>` and serializing it according to the `Vec` commitment serialization rules.
+/// This operation is internally performed via conversion into `BTreeMap<usize, T: Commitment>`.
+impl<T> Commitment for HashMap<usize, T> where T: Commitment + Clone {
+    fn commitment_serialize<E: io::Write>(&self, mut e: E) -> Result<usize, Error> {
+        let ordered: BTreeMap<usize, T> = self.iter().map(|(key , val)| {
+            (*key, val.clone())
+        }).collect();
+        ordered.commitment_serialize(&mut e)
+    }
+
+    fn commitment_deserialize<D: io::Read>(mut d: D) -> Result<Self, Error> {
+        let map: HashMap<usize, T> =
+            BTreeMap::<usize, T>::commitment_deserialize(&mut d)?.iter().map(|(key, val)| {
+                (*key, val.clone())
+            }).collect();
+        Ok(map)
+    }
+}
+
+/// LNP/BP library uses `BTreeMap<usize, T: Commitment>`s to serialize ordered lists, where the
+/// position of the list item must be fixed, since the item is referenced from elsewhere by its
+/// index. Thus, the library does not supports and recommends not to support commitment
+/// serialization of any other `BTreeMap` variants.
+///
+/// Commitment serialization of the `BTreeMap<usize, T>` type is performed by converting into
+/// a fixed-order `Vec<T>` and serializing it according to the `Vec` commitment serialization rules.
+impl<T> Commitment for BTreeMap<usize, T> where T: Commitment {
     fn commitment_serialize<E: io::Write>(&self, mut e: E) -> Result<usize, Error> {
         let len = self.len() as usize;
         let serialized = len.commitment_serialize(&mut e)?;
 
-        let ordered: BTreeMap<_,_> = self.iter().collect();
-        ordered.values().try_fold(serialized, |acc, item| {
+        self.values().try_fold(serialized, |acc, item| {
             item.commitment_serialize(&mut e).map(|len| acc + len)
         })
     }
 
     fn commitment_deserialize<D: io::Read>(mut d: D) -> Result<Self, Error> {
         let len = usize::commitment_deserialize(&mut d)?;
-        let mut map = HashMap::<usize, T>::new();
+        let mut map = BTreeMap::<usize, T>::new();
         for index in 0..len {
             map.insert(index, T::commitment_deserialize(&mut d)?);
         }
@@ -93,12 +140,13 @@ mod tests {
     use bitcoin::Transaction;
     impl FromConsensus for Transaction { }
 
+    /// Test for checking the following rule from LNPBP-5:
+    ///
+    /// `Option<T>` of any type T, which are set to `Option::None` value MUST serialize as a two
+    /// zero bytes and it MUST be possible to deserialize optional of any type from two zero bytes
+    /// which MUST result in `Option::None` value.
     #[test]
-    /// Optionals of any type set to None must serialize to a two zero bytes and
-    /// it must be possible to deserialize optional of any type from two zero bytes which
-    /// must result of None value.
     fn test_option_serialize_none() {
-
         let o1: Option<u8> = None;
         let o2: Option<u64> = None;
         let o3: Option<Transaction> = None;
