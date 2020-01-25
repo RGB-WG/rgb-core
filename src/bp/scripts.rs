d0ef6151d45761541ea220140047538f592f98dd
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-01-25 21:22:04 +0100

    Adopting new Miniscript key iterator

--- src/bp/scripts.rs
+++ src/bp/scripts.rs
@@ -75,12 +75,8 @@
 //! * `--(#=type)--`: the hash of the value following `->` must match to the value of the `<type>`
 //!
 
-use bitcoin::{
-    hash_types::*,
-    blockdata::script::*,
-    secp256k1
-};
-use miniscript::{Miniscript, Terminal::*, MiniscriptKey};
+use bitcoin::{hash_types::*, blockdata::script::*, secp256k1};
+use miniscript::{Miniscript, MiniscriptKey, miniscript::iter::KeyOrHash};
 use crate::Wrapper;
 
 
@@ -114,49 +110,15 @@ impl<Pk: MiniscriptKey> From<miniscript::Error> for LockScriptParseError<Pk> {
 
 impl LockScript {
     pub fn extract_pubkeys(&self) -> Result<Vec<secp256k1::PublicKey>, LockScriptParseError<bitcoin::PublicKey>> {
-        let miniscript = Miniscript::parse(&self.clone().into_inner())?;
-        Ok(Self::extract_pubkeys_from_terminal(&miniscript)?.iter().map(|bpk| bpk.key).collect())
-    }
-
-    fn extract_pubkeys_from_terminal(
-        miniscript: &Miniscript<miniscript::bitcoin::PublicKey>
-    ) -> Result<Vec<bitcoin::PublicKey>, LockScriptParseError<bitcoin::PublicKey>> {
-        Ok(match &miniscript.node {
-            Pk(key) => vec![key.clone()],
-            PkH(pkh) => Err(LockScriptParseError::PubkeyHash(pkh.clone()))?,
-            ThreshM(_, keys) =>
-                keys.iter().map(|key| key.clone()).collect(),
-
-            Alt(node) |
-            Swap(node) |
-            Check(node) |
-            DupIf(node) |
-            Verify(node) |
-            NonZero(node) |
-            ZeroNotEqual(node) =>
-                Self::extract_pubkeys_from_terminal(&node)?,
-            AndV(node1, node2) |
-            AndB(node1, node2) |
-            OrB(node1, node2) |
-            OrD(node1, node2) |
-            OrC(node1, node2) |
-            OrI(node1, node2) =>
-                Self::extract_pubkeys_from_terminal(&node1)?.into_iter()
-                    .chain(Self::extract_pubkeys_from_terminal(&node2)?.into_iter()).collect(),
-            AndOr(node1, node2, node3) =>
-                Self::extract_pubkeys_from_terminal(&node1)?.into_iter()
-                    .chain(Self::extract_pubkeys_from_terminal(&node2)?.into_iter())
-                    .chain(Self::extract_pubkeys_from_terminal(&node3)?.into_iter()).collect(),
-            Thresh(_, node_vec) => {
-                let mut vec= vec![];
-                for node in node_vec {
-                    vec.append(&mut Self::extract_pubkeys_from_terminal(&node)?);
-                }
-                vec
-            }
-
-            _ => vec![],
-        })
+        Miniscript::parse(&self.clone().into_inner())?
+            .iter_keys_and_hashes()
+            .try_fold(Vec::<secp256k1::PublicKey>::new(), |mut keys, item| match item {
+                KeyOrHash::HashedPubkey(hash) => Err(LockScriptParseError::PubkeyHash(hash)),
+                KeyOrHash::PlainPubkey(key) => {
+                    keys.push(key.key);
+                    Ok(keys)
+                },
+            })
     }
 }
 
