716c30ac2329cac55f0c0dd02be7bdf658ee1724
Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
2020-05-26 14:18:10 +0200

    LNP: Local node key management

--- src/lnp/session/local_node.rs
+++ src/lnp/session/local_node.rs
@@ -11,15 +11,45 @@
 // along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
-use bitcoin::secp256k1;
+use bitcoin::secp256k1::{self, All, Secp256k1};
+#[cfg(feature = "keygen")]
+use rand::thread_rng;
 
 //use super::{NodeAddr, NodeLocator, Session, SessionTrait};
 
 #[derive(Clone, PartialEq, Eq, Debug, Display)]
 #[display_from(Debug)]
 pub struct LocalNode {
-    private_key: Option<secp256k1::SecretKey>,
-    ephemeral_private_key: Option<secp256k1::SecretKey>,
+    secp: Secp256k1<All>,
+    private_key: secp256k1::SecretKey,
+    ephemeral_private_key: secp256k1::SecretKey,
+}
+
+impl LocalNode {
+    #[cfg(feature = "keygen")]
+    pub fn new() -> Self {
+        let secp = Secp256k1::new();
+        let mut rng = thread_rng();
+        let private_key = secp256k1::SecretKey::new(&mut rng);
+        let ephemeral_private_key = secp256k1::SecretKey::new(&mut rng);
+        Self {
+            secp,
+            private_key,
+            ephemeral_private_key,
+        }
+    }
+
+    pub fn from_keys(node_key: secp256k1::SecretKey, ephemeral_key: secp256k1::SecretKey) -> Self {
+        Self {
+            secp: Secp256k1::new(),
+            private_key: node_key,
+            ephemeral_private_key: ephemeral_key,
+        }
+    }
+
+    pub fn node_id(&self) -> secp256k1::PublicKey {
+        secp256k1::PublicKey::from_secret_key(&self.secp, &self.private_key)
+    }
 }
 
 /*
