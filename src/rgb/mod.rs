LNP-BP/rust-lnpbp

[wip] some graph stuff

src/rgb/mod.rs
@@ -21,6 +21,7 @@ pub mod seal;
 pub mod state;
 pub mod script;
 pub mod transition;
+pub mod validation;
 
 pub mod serialize;
 pub mod commit;

RGB-WG/rgb-core

src/rgb/mod.rs
@@ -21,6 +21,7 @@ pub mod seal;
 pub mod state;
 pub mod script;
 pub mod transition;
+pub mod history;
 
 pub mod serialize;
 pub mod commit;