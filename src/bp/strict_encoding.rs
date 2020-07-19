dbd0d324bf7762e8ddb7c88393ad70df5a96dc6e
codeShark149 <rajarshi149@gmail.com>
2020-07-19 18:36:41 +0530

    Fixed read buffer size for Signature

--- src/bp/strict_encoding.rs
+++ src/bp/strict_encoding.rs
@@ -100,7 +100,7 @@ impl StrictDecode for secp256k1::Signature {
 
     #[inline]
     fn strict_decode<D: io::Read>(mut d: D) -> Result<Self, Self::Error> {
-        let mut buf = [0u8; secp256k1::constants::PUBLIC_KEY_SIZE];
+        let mut buf = [0u8; secp256k1::constants::COMPACT_SIGNATURE_SIZE];
         d.read_exact(&mut buf)?;
         Ok(Self::from_compact(&buf).map_err(|_| {
             Error::DataIntegrityError("Invalid secp256k1 signature data".to_string())
