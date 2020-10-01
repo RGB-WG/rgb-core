4d7478df6bdff07970043cd4f2e4d104a881c844
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-10-01 05:36:17 +0200

    Feature flags finalization, formatting & tests

--- src/common/feature.rs
+++ src/common/feature.rs
@@ -15,10 +15,12 @@
 // If not, see <https://opensource.org/licenses/MIT>.
 
 use std::cmp::max;
-use std::fmt::{self, Debug, Display, Formatter};
+use std::convert::{TryFrom, TryInto};
+use std::fmt::{self, Binary, Debug, Display, Formatter, LowerHex, Octal, UpperHex};
 use std::hash::{Hash, Hasher};
 use std::io;
 use std::ops::{BitAnd, BitOr, BitXor};
+use std::str::FromStr;
 
 use crate::paradigms::strict_encoding::StrictDecode;
 use crate::strict_encoding::StrictEncode;
@@ -46,7 +48,7 @@ impl BitOr for FlagVec {
         lhs.enlarge(size);
         rhs.enlarge(size);
         for i in 0..rhs.0.len() {
-            rhs.0[i] = lhs.0[1] | rhs.0[i];
+            rhs.0[i] = lhs.0[i] | rhs.0[i];
         }
         rhs
     }
@@ -61,7 +63,7 @@ impl BitAnd for FlagVec {
         lhs.enlarge(size);
         rhs.enlarge(size);
         for i in 0..rhs.0.len() {
-            rhs.0[i] = lhs.0[1] & rhs.0[i];
+            rhs.0[i] = lhs.0[i] & rhs.0[i];
         }
         rhs
     }
@@ -76,7 +78,7 @@ impl BitXor for FlagVec {
         lhs.enlarge(size);
         rhs.enlarge(size);
         for i in 0..rhs.0.len() {
-            rhs.0[i] = lhs.0[1] ^ rhs.0[i];
+            rhs.0[i] = lhs.0[i] ^ rhs.0[i];
         }
         rhs
     }
@@ -104,9 +106,13 @@ impl Hash for FlagVec {
 
 impl Debug for FlagVec {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.write_str("flags:")?;
-        for b in 0..self.capacity() {
-            write!(f, "{}", if self.is_set(b) { 1 } else { 0 })?;
+        let mut shrunk = self.clone();
+        if f.alternate() {
+            f.write_str("flags:")?;
+            shrunk.shrink();
+        }
+        for b in 0..shrunk.capacity() {
+            write!(f, "{}", if self.is_set(b) { '1' } else { '0' })?;
         }
         Ok(())
     }
@@ -114,11 +120,114 @@ impl Debug for FlagVec {
 
 impl Display for FlagVec {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let cap = self.shrunk().capacity();
-        for b in 0..cap {
-            write!(f, "{}", if self.is_set(b) { 1 } else { 0 })?;
+        let mut shrunk = self.clone();
+        let (p, n) = if f.alternate() {
+            shrunk.shrink();
+            ('#', '_')
+        } else {
+            ('+', '-')
+        };
+        for b in 0..shrunk.capacity() {
+            write!(f, "{}", if shrunk.is_set(b) { p } else { n })?;
         }
         Ok(())
+    }
+}
+
+impl LowerHex for FlagVec {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut shrunk = self.clone();
+        if f.alternate() {
+            f.write_str("0x")?;
+            shrunk.shrink();
+        }
+        for b in shrunk.0 {
+            write!(f, "{:02x}", b)?;
+        }
+        Ok(())
+    }
+}
+
+impl UpperHex for FlagVec {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut shrunk = self.clone();
+        if f.alternate() {
+            f.write_str("0x")?;
+            shrunk.shrink();
+        }
+        for b in shrunk.0 {
+            write!(f, "{:02X}", b)?;
+        }
+        Ok(())
+    }
+}
+
+impl Binary for FlagVec {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut shrunk = self.clone();
+        if f.alternate() {
+            f.write_str("0b")?;
+            shrunk.shrink();
+        }
+        for b in shrunk.0 {
+            write!(f, "{:08b}", b)?;
+        }
+        Ok(())
+    }
+}
+
+impl Octal for FlagVec {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut shrunk = self.clone();
+        if f.alternate() {
+            f.write_str("0o")?;
+            shrunk.shrink();
+        }
+        for b in shrunk.0 {
+            write!(f, "{:03o}", b)?;
+        }
+        Ok(())
+    }
+}
+
+impl FromStr for FlagVec {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut vec = Vec::with_capacity(s.len());
+        for c in s.chars() {
+            vec.push(match c {
+                '1' | '+' | '*' | '#' => 1,
+                '0' | '-' | '!' | '_' => 0,
+                ' ' | '\n' | '\t' | '\r' => continue,
+                _ => return Err(()),
+            })
+        }
+        vec.try_into()
+    }
+}
+
+impl TryFrom<Vec<u8>> for FlagVec {
+    type Error = ();
+
+    fn try_from(value: Vec<u8>) -> Result<Self, Self::Error> {
+        Self::try_from(&value[..])
+    }
+}
+
+impl TryFrom<&[u8]> for FlagVec {
+    type Error = ();
+
+    fn try_from(value: &[u8]) -> Result<Self, Self::Error> {
+        let mut vec = FlagVec::with_capacity(value.len() as FlagNo);
+        for (i, v) in value.iter().enumerate() {
+            match v {
+                1 => vec.set(i as FlagNo),
+                0 => false,
+                _ => return Err(()),
+            };
+        }
+        Ok(vec)
     }
 }
 
@@ -135,9 +244,26 @@ impl StrictDecode for FlagVec {
 }
 
 impl FlagVec {
+    fn bits_to_bytes(bits: FlagNo) -> usize {
+        if bits == 0 {
+            0
+        } else {
+            ((bits - 1) / 8 + 1) as usize
+        }
+    }
+
     /// Constructs a features vector of zero feature flag set
     pub fn new() -> FlagVec {
         FlagVec(vec![])
+    }
+
+    /// Constructs a features vector of `upto` feature flag in unset state
+    pub fn with_capacity(upto: FlagNo) -> Self {
+        if upto == 0 {
+            FlagVec::default()
+        } else {
+            FlagVec(vec![0u8; Self::bits_to_bytes(upto)])
+        }
     }
 
     /// Returns a shrunk copy of the self
@@ -190,8 +316,8 @@ impl FlagVec {
         }
 
         let old = self.0.clone();
-        self.0 = vec![0u8; ((upto + 1) / 8) as usize];
-        self.0.copy_from_slice(&old);
+        self.0 = vec![0u8; Self::bits_to_bytes(upto)];
+        self.0[..old.len()].copy_from_slice(&old);
         return true;
     }
 
@@ -202,11 +328,15 @@ impl FlagVec {
     #[inline]
     pub fn shrink(&mut self) -> bool {
         let capacity = self.capacity();
+        if capacity == 0 {
+            return false;
+        }
         let mut top = 1;
         while top < capacity && !self.is_set(capacity - top) {
             top += 1;
         }
-        let used = ((top + 1) / 8) as usize;
+        let top = capacity - top;
+        let used = Self::bits_to_bytes(top);
         if used < self.0.len() {
             let old = self.0.clone();
             self.0 = vec![0u8; used as usize];
@@ -220,7 +350,7 @@ impl FlagVec {
     /// If the maximum capacity is exceeded, returns [`Option::None`].
     #[inline]
     fn byte_at(&self, flag_no: FlagNo) -> Option<&u8> {
-        if flag_no > self.capacity() {
+        if flag_no >= self.capacity() {
             return None;
         }
         Some(&self.0[flag_no as usize / 8])
@@ -255,7 +385,7 @@ impl FlagVec {
     }
 
     /// Unsets the feature flag with number `flag_no`. Returns `true` if the
-    /// flag was already set and `false` otherwise (i.e. previous value of the
+    /// flag was set before and `false` otherwise (i.e. previous value of the
     /// flag)
     #[inline]
     pub fn unset(&mut self, flag_no: FlagNo) -> bool {
@@ -263,7 +393,7 @@ impl FlagVec {
         let mask = 1u8 << (flag_no % 8);
         let was = *byte & mask;
         *byte = *byte & (!mask);
-        was == 0
+        was > 0
     }
 
     /// Inverts the feature flag with number `flag_no`
@@ -350,5 +480,152 @@ impl Iterator for FilteredIter<'_> {
             self.offset += 1;
         }
         None
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_flag_init() {
+        let empty_vec = Vec::<FlagNo>::new();
+
+        let f1 = FlagVec::default();
+        let f2 = FlagVec::new();
+        let f3 = FlagVec::with_capacity(0);
+        assert_eq!(f1, f2);
+        assert_eq!(f1, f3);
+        assert_eq!(f2, f3);
+        assert_eq!(f1.capacity(), 0);
+        assert_eq!(f1.iter().collect::<Vec<_>>(), empty_vec);
+        assert_eq!(f2.capacity(), 0);
+        assert_eq!(f2.iter().collect::<Vec<_>>(), empty_vec);
+        assert_eq!(f3.capacity(), 0);
+        assert_eq!(f3.iter().collect::<Vec<_>>(), empty_vec);
+
+        let f4 = FlagVec::with_capacity(10);
+        assert_eq!(f1, f4);
+        assert_eq!(f4.capacity(), 16);
+        assert_eq!(f4.iter().collect::<Vec<_>>(), empty_vec);
+    }
+
+    #[test]
+    fn test_flag_capacity() {
+        let mut f1 = FlagVec::with_capacity(10);
+        assert_eq!(f1.capacity(), 16);
+        let mut f2 = f1.clone();
+        assert_eq!(f2.capacity(), 16);
+        f2 = f1.shrunk();
+        f1.shrink();
+        assert_eq!(f1.capacity(), 0);
+        assert_eq!(f1, f2);
+        f1.enlarge(20);
+        assert_eq!(f1.capacity(), 24);
+    }
+
+    #[test]
+    fn test_flag_ops() {
+        let mut f1 = FlagVec::with_capacity(10);
+        assert_eq!(f1.is_set(33), false);
+        assert_eq!(f1.capacity(), 16);
+        assert_eq!(f1.set(2), false);
+        assert_eq!(f1.is_set(2), true);
+        assert_eq!(f1.unset(2), true);
+        assert_eq!(f1.is_set(2), false);
+        assert_eq!(f1.set(7), false);
+        f1.shrink();
+        assert_eq!(f1.capacity(), 8);
+        assert_eq!(f1.set(22), false);
+        assert_eq!(f1.is_set(22), true);
+        assert_eq!(f1.capacity(), 24);
+        assert_eq!(f1.invert(22), ());
+        assert_eq!(f1.is_set(22), false);
+        assert_eq!(f1.capacity(), 24);
+        f1.shrink();
+        assert_eq!(f1.capacity(), 8);
+    }
+
+    #[test]
+    fn test_fmt() {
+        let mut f1 = FlagVec::from_str("-0-\t#__1 \n--\r+* +!").unwrap();
+        assert_eq!(f1.is_set(0), false);
+        assert_eq!(f1.is_set(1), false);
+        assert_eq!(f1.is_set(2), false);
+        assert_eq!(f1.is_set(3), true);
+        assert_eq!(f1.is_set(4), false);
+        assert_eq!(f1.is_set(5), false);
+        assert_eq!(f1.is_set(6), true);
+        assert_eq!(f1.is_set(7), false);
+        assert_eq!(f1.is_set(8), false);
+        assert_eq!(f1.is_set(9), true);
+        assert_eq!(f1.is_set(10), true);
+        assert_eq!(f1.is_set(11), true);
+        assert_eq!(f1.is_set(12), false);
+        assert_eq!(f1.capacity(), 16);
+        assert_eq!(format!("{}", f1), "---+--+--+++----");
+        assert_eq!(format!("{:#}", f1), "___#__#__###____");
+        assert_eq!(format!("{:?}", f1), "0001001001110000");
+        assert_eq!(format!("{:x}", f1), "480e");
+        assert_eq!(format!("{:X}", f1), "480E");
+        assert_eq!(format!("{:b}", f1), "0100100000001110");
+        assert_eq!(format!("{:o}", f1), "110016");
+        f1.enlarge(22);
+        assert_eq!(format!("{}", f1), "---+--+--+++------------");
+        assert_eq!(format!("{:#}", f1), "___#__#__###____");
+        assert_eq!(format!("{:?}", f1), "000100100111000000000000");
+        assert_eq!(format!("{:#?}", f1), "flags:0001001001110000");
+        assert_eq!(format!("{:#x}", f1), "0x480e");
+        assert_eq!(format!("{:#X}", f1), "0x480E");
+        assert_eq!(format!("{:#b}", f1), "0b0100100000001110");
+        assert_eq!(format!("{:#o}", f1), "0o110016");
+    }
+
+    #[test]
+    fn test_filtered() {
+        let f1 = FlagVec::from_str("---+--+--+++-").unwrap();
+        let f2 = FlagVec::from_str("-+++-+---+-++--+").unwrap();
+        assert_eq!(
+            f2.iter().collect::<Vec<_>>(),
+            vec![1u16, 2, 3, 5, 9, 11, 12, 15]
+        );
+        assert_eq!(
+            f2.known_iter(f1.clone()).collect::<Vec<_>>(),
+            vec![3u16, 9, 11]
+        );
+        assert_eq!(
+            f2.unknown_iter(f1).collect::<Vec<_>>(),
+            vec![1u16, 2, 5, 9, 12, 15]
+        );
+    }
+
+    #[test]
+    fn test_binary_and() {
+        let f1 = FlagVec::from_str("---+--+--+++-").unwrap();
+        let f2 = FlagVec::from_str("-+++-+---+-++--+").unwrap();
+        assert_eq!(
+            f1.clone() & f2.clone(),
+            FlagVec::from_str("---+-----+-+----").unwrap()
+        );
+    }
+
+    #[test]
+    fn test_binary_or() {
+        let f1 = FlagVec::from_str("---+--+--+++-").unwrap();
+        let f2 = FlagVec::from_str("-+++-+---+-++--+").unwrap();
+        assert_eq!(
+            f1.clone() | f2.clone(),
+            FlagVec::from_str("-+++-++--++++--+").unwrap()
+        );
+    }
+
+    #[test]
+    fn test_binary_xor() {
+        let f1 = FlagVec::from_str("---+--+--+++-").unwrap();
+        let f2 = FlagVec::from_str("-+++-+---+-++--+").unwrap();
+        assert_eq!(
+            f1.clone() ^ f2.clone(),
+            FlagVec::from_str("-++--++---+-+--+").unwrap()
+        );
     }
 }
