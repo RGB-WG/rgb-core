6e8f9c2681d0995fd787269e4d52a55109479145
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-10-28 21:59:06 +0100

    LN types formatting implementations

--- src/lnp/application/mod.rs
+++ src/lnp/application/mod.rs
@@ -26,16 +26,21 @@ pub use peer_connection::{
 };
 pub use rpc_connection::RpcConnection;
 
+use bitcoin::hashes::hex::{Error, FromHex, ToHex};
 use bitcoin::hashes::{sha256, Hmac};
-use bitcoin_hashes::hex::{Error, FromHex};
+use std::fmt::{self, Formatter, LowerHex, UpperHex};
 
-/// Lightning network channel Id
+// TODO: (new) Move type to rust-amplify
+/// Wrapper type for all slice-based 256-bit types implementing many important
+/// traits, so types based on it can simply derive their implementations
 #[derive(
     Wrapper,
     Clone,
     Copy,
     PartialEq,
     Eq,
+    PartialOrd,
+    Ord,
     Hash,
     Debug,
     Display,
@@ -44,8 +49,79 @@ use bitcoin_hashes::hex::{Error, FromHex};
     StrictDecode,
 )]
 #[lnpbp_crate(crate)]
-#[display(Debug)]
-pub struct ChannelId([u8; 32]);
+#[display(LowerHex)]
+pub struct Slice32([u8; 32]);
+
+impl FromHex for Slice32 {
+    fn from_byte_iter<I>(iter: I) -> Result<Self, Error>
+    where
+        I: Iterator<Item = Result<u8, Error>>
+            + ExactSizeIterator
+            + DoubleEndedIterator,
+    {
+        let vec = Vec::<u8>::from_byte_iter(iter)?;
+        if vec.len() != 32 {
+            return Err(Error::InvalidLength(32, vec.len()));
+        }
+        let mut id = [0u8; 32];
+        id.copy_from_slice(&vec);
+        Ok(Slice32(id))
+    }
+}
+
+impl LowerHex for Slice32 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "0x{}", self.to_hex())
+        } else {
+            f.write_str(&self.to_hex())
+        }
+    }
+}
+
+impl UpperHex for Slice32 {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        if f.alternate() {
+            write!(f, "0x{}", self.to_hex().to_ascii_uppercase())
+        } else {
+            f.write_str(&self.to_hex().to_ascii_uppercase())
+        }
+    }
+}
+
+/*
+impl<Idx> Index<Idx> for Slice32
+where
+    Idx: std::slice::SliceIndex<[u8]>,
+{
+    type Output = ();
+
+    fn index(&self, index: Idx) -> &Self::Output {
+        self.0[index]
+    }
+}
+ */
+
+/// Lightning network channel Id
+#[derive(
+    Wrapper,
+    Clone,
+    Copy,
+    PartialEq,
+    Eq,
+    PartialOrd,
+    Ord,
+    Hash,
+    Debug,
+    Display,
+    From,
+    StrictEncode,
+    StrictDecode,
+)]
+#[lnpbp_crate(crate)]
+#[display(LowerHex)]
+#[wrapper(LowerHex, UpperHex)]
+pub struct ChannelId(Slice32);
 
 impl FromHex for ChannelId {
     fn from_byte_iter<I>(iter: I) -> Result<Self, Error>
@@ -54,7 +130,7 @@ impl FromHex for ChannelId {
             + ExactSizeIterator
             + DoubleEndedIterator,
     {
-        Ok(ChannelId(slice32_from_byte_iter(iter)?))
+        Ok(Self(Slice32::from_byte_iter(iter)?))
     }
 }
 
@@ -65,6 +141,8 @@ impl FromHex for ChannelId {
     Copy,
     PartialEq,
     Eq,
+    PartialOrd,
+    Ord,
     Hash,
     Debug,
     Display,
@@ -73,8 +151,9 @@ impl FromHex for ChannelId {
     StrictDecode,
 )]
 #[lnpbp_crate(crate)]
-#[display(Debug)]
-pub struct TempChannelId([u8; 32]);
+#[display(LowerHex)]
+#[wrapper(LowerHex, UpperHex)]
+pub struct TempChannelId(Slice32);
 
 impl FromHex for TempChannelId {
     fn from_byte_iter<I>(iter: I) -> Result<Self, Error>
@@ -83,7 +162,7 @@ impl FromHex for TempChannelId {
             + ExactSizeIterator
             + DoubleEndedIterator,
     {
-        Ok(TempChannelId(slice32_from_byte_iter(iter)?))
+        Ok(Self(Slice32::from_byte_iter(iter)?))
     }
 }
 
@@ -94,6 +173,8 @@ impl FromHex for TempChannelId {
     Copy,
     PartialEq,
     Eq,
+    PartialOrd,
+    Ord,
     Hash,
     Debug,
     Display,
@@ -102,8 +183,9 @@ impl FromHex for TempChannelId {
     StrictDecode,
 )]
 #[lnpbp_crate(crate)]
-#[display(Debug)]
-pub struct PaymentHash([u8; 32]);
+#[display(LowerHex)]
+#[wrapper(LowerHex, UpperHex)]
+pub struct PaymentHash(Slice32);
 
 impl FromHex for PaymentHash {
     fn from_byte_iter<I>(iter: I) -> Result<Self, Error>
@@ -112,7 +194,7 @@ impl FromHex for PaymentHash {
             + ExactSizeIterator
             + DoubleEndedIterator,
     {
-        Ok(PaymentHash(slice32_from_byte_iter(iter)?))
+        Ok(Self(Slice32::from_byte_iter(iter)?))
     }
 }
 
@@ -123,6 +205,8 @@ impl FromHex for PaymentHash {
     Copy,
     PartialEq,
     Eq,
+    PartialOrd,
+    Ord,
     Hash,
     Debug,
     Display,
@@ -131,8 +215,9 @@ impl FromHex for PaymentHash {
     StrictDecode,
 )]
 #[lnpbp_crate(crate)]
-#[display(Debug)]
-pub struct PaymentPreimage([u8; 32]);
+#[display(LowerHex)]
+#[wrapper(LowerHex, UpperHex)]
+pub struct PaymentPreimage(Slice32);
 
 impl FromHex for PaymentPreimage {
     fn from_byte_iter<I>(iter: I) -> Result<Self, Error>
@@ -141,7 +226,7 @@ impl FromHex for PaymentPreimage {
             + ExactSizeIterator
             + DoubleEndedIterator,
     {
-        Ok(PaymentPreimage(slice32_from_byte_iter(iter)?))
+        Ok(Self(Slice32::from_byte_iter(iter)?))
     }
 }
 
@@ -153,6 +238,8 @@ impl FromHex for PaymentPreimage {
     Copy,
     PartialEq,
     Eq,
+    PartialOrd,
+    Ord,
     Hash,
     Debug,
     Display,
@@ -161,8 +248,9 @@ impl FromHex for PaymentPreimage {
     StrictDecode,
 )]
 #[lnpbp_crate(crate)]
-#[display(Debug)]
-pub struct PaymentSecret([u8; 32]);
+#[display(LowerHex)]
+#[wrapper(LowerHex, UpperHex)]
+pub struct PaymentSecret(Slice32);
 
 impl FromHex for PaymentSecret {
     fn from_byte_iter<I>(iter: I) -> Result<Self, Error>
@@ -171,7 +259,7 @@ impl FromHex for PaymentSecret {
             + ExactSizeIterator
             + DoubleEndedIterator,
     {
-        Ok(PaymentSecret(slice32_from_byte_iter(iter)?))
+        Ok(Self(Slice32::from_byte_iter(iter)?))
     }
 }
 
@@ -183,19 +271,4 @@ pub struct OnionPacket {
     pub public_key: bitcoin::secp256k1::PublicKey,
     pub hop_data: Vec<u8>, //[u8; 20 * 65],
     pub hmac: Hmac<sha256::Hash>,
-}
-
-fn slice32_from_byte_iter<I>(iter: I) -> Result<[u8; 32], Error>
-where
-    I: Iterator<Item = Result<u8, Error>>
-        + ExactSizeIterator
-        + DoubleEndedIterator,
-{
-    let vec = Vec::<u8>::from_byte_iter(iter)?;
-    if vec.len() != 32 {
-        return Err(Error::InvalidLength(32, vec.len()));
-    }
-    let mut id = [0u8; 32];
-    id.copy_from_slice(&vec);
-    Ok(id)
 }
