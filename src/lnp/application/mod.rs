75c546791732651c628dc3f9dd40c222061e484a
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-11-04 16:49:52 +0100

    Separating invoice module in LNP

--- src/lnp/application/mod.rs
+++ src/lnp/application/mod.rs
@@ -11,10 +11,10 @@
 // along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
-pub extern crate lightning_invoice as invoice;
-
 pub mod channel;
+// pub mod extensions;
 mod features;
+pub mod invoice;
 pub mod message;
 pub mod peer_connection;
 pub mod rpc_connection;
@@ -25,40 +25,9 @@ pub use channel::{
     ChannelParams, ChannelState, TempChannelId,
 };
 pub use features::{FeatureContext, FeatureFlag, Features};
+pub use invoice::Invoice;
 pub use message::{Messages, OnionPacket, LNPWP_UNMARSHALLER};
 pub use peer_connection::{
     PeerConnection, PeerReceiver, PeerSender, RecvMessage, SendMessage,
 };
 pub use rpc_connection::RpcConnection;
-
-use invoice::Invoice;
-use std::io;
-use std::str::FromStr;
-
-use self::invoice::ParseOrSemanticError;
-use crate::strict_encoding::{self, StrictDecode, StrictEncode};
-
-impl StrictEncode for Invoice {
-    type Error = strict_encoding::Error;
-    #[inline]
-    fn strict_encode<E: io::Write>(&self, e: E) -> Result<usize, Self::Error> {
-        self.to_string().strict_encode(e)
-    }
-}
-
-impl StrictDecode for Invoice {
-    type Error = strict_encoding::Error;
-    #[inline]
-    fn strict_decode<D: io::Read>(d: D) -> Result<Self, Self::Error> {
-        Self::from_str(&String::strict_decode(d)?).map_err(|e| {
-            // TODO: (v0.3) this can be improved once PR got merged:
-            //       <https://github.com/rust-bitcoin/rust-lightning-invoice/pull/43>
-            strict_encoding::Error::DataIntegrityError(match e {
-                ParseOrSemanticError::ParseError(err) => err.to_string(),
-                ParseOrSemanticError::SemanticError(err) => {
-                    s!("Lightning invoice semantic error")
-                }
-            })
-        })
-    }
-}
