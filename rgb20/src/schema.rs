064c0d8a0ee5f0b4bb9e0e4eed000f86d2ba63fa
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-01-24 11:04:28 +0100

    RGB20 schema tests

--- rgb20/src/schema.rs
+++ rgb20/src/schema.rs
@@ -434,3 +434,322 @@ impl Deref for HistoryProofFormat {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use lnpbp::strict_encoding::{StrictDecode, StrictEncode};
+    use rgb::bech32::ToBech32Data;
+    use rgb::{FromBech32, ToBech32};
+
+    #[test]
+    fn schema_id() {
+        let id = schema().schema_id();
+        println!("{}", id);
+        assert_eq!(
+            id.to_string(),
+            "sch1zcdeayj9vpv852tx2sjzy7esyy82a6nk0gs854ktam24zxee42rqyzg95g"
+        );
+    }
+
+    #[test]
+    fn schema_structure() {
+        let debug = format!("{:#?}", schema());
+        assert_eq!(
+            debug,
+            "Schema {
+    rgb_features: flags:,
+    root_id: 0000000000000000000000000000000000000000000000000000000000000000,
+    field_types: {
+        0: String(
+            8,
+        ),
+        1: String(
+            256,
+        ),
+        2: String(
+            65535,
+        ),
+        3: Unsigned(
+            Bit8,
+            0,
+            18,
+        ),
+        4: Integer(
+            Bit64,
+            1602340666,
+            9223372036854775807,
+        ),
+        160: Unsigned(
+            Bit64,
+            0,
+            18446744073709551615,
+        ),
+        176: Unsigned(
+            Bit64,
+            0,
+            18446744073709551615,
+        ),
+        177: TxOutPoint,
+        178: Bytes(
+            65535,
+        ),
+        179: Enum(
+            {
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+            },
+        ),
+    },
+    owned_right_types: {
+        1: StateSchema {
+            format: Declarative,
+            abi: {},
+        },
+        160: StateSchema {
+            format: CustomData(
+                Unsigned(
+                    Bit64,
+                    0,
+                    18446744073709551615,
+                ),
+            ),
+            abi: {},
+        },
+        161: StateSchema {
+            format: DiscreteFiniteField(
+                Unsigned64bit,
+            ),
+            abi: {
+                Validate: Embedded(
+                    NoInflationBySum,
+                ),
+            },
+        },
+        170: StateSchema {
+            format: Declarative,
+            abi: {},
+        },
+        171: StateSchema {
+            format: Declarative,
+            abi: {},
+        },
+    },
+    public_right_types: {},
+    genesis: GenesisSchema {
+        metadata: {
+            0: Once,
+            1: Once,
+            2: NoneOrOnce,
+            3: Once,
+            4: Once,
+            160: Once,
+        },
+        owned_rights: {
+            1: NoneOrOnce,
+            160: NoneOrMore,
+            161: NoneOrMore,
+            170: NoneOrOnce,
+        },
+        public_rights: {},
+        abi: {},
+    },
+    extensions: {},
+    transitions: {
+        0: TransitionSchema {
+            metadata: {},
+            closes: {
+                161: OnceOrMore,
+            },
+            owned_rights: {
+                161: NoneOrMore,
+            },
+            public_rights: {},
+            abi: {},
+        },
+        16: TransitionSchema {
+            metadata: {
+                0: NoneOrOnce,
+                1: NoneOrOnce,
+                2: NoneOrOnce,
+                3: NoneOrOnce,
+            },
+            closes: {
+                1: Once,
+            },
+            owned_rights: {
+                1: NoneOrOnce,
+            },
+            public_rights: {},
+            abi: {},
+        },
+        160: TransitionSchema {
+            metadata: {
+                160: Once,
+            },
+            closes: {
+                160: OnceOrMore,
+            },
+            owned_rights: {
+                160: NoneOrMore,
+                161: NoneOrMore,
+                170: NoneOrOnce,
+            },
+            public_rights: {},
+            abi: {
+                Validate: Embedded(
+                    FungibleInflation,
+                ),
+            },
+        },
+        161: TransitionSchema {
+            metadata: {},
+            closes: {
+                170: Once,
+            },
+            owned_rights: {
+                170: NoneOrOnce,
+                171: NoneOrOnce,
+            },
+            public_rights: {},
+            abi: {},
+        },
+        162: TransitionSchema {
+            metadata: {
+                176: Once,
+                177: OnceOrMore,
+                178: NoneOrMore,
+                179: Once,
+            },
+            closes: {
+                171: Once,
+            },
+            owned_rights: {
+                171: NoneOrOnce,
+            },
+            public_rights: {},
+            abi: {
+                Validate: Embedded(
+                    ProofOfBurn,
+                ),
+            },
+        },
+        163: TransitionSchema {
+            metadata: {
+                176: Once,
+                177: OnceOrMore,
+                178: NoneOrMore,
+                179: Once,
+            },
+            closes: {
+                171: Once,
+            },
+            owned_rights: {
+                161: OnceOrMore,
+                171: NoneOrOnce,
+            },
+            public_rights: {},
+            abi: {
+                Validate: Embedded(
+                    ProofOfBurn,
+                ),
+            },
+        },
+        240: TransitionSchema {
+            metadata: {},
+            closes: {
+                1: NoneOrOnce,
+                160: NoneOrMore,
+                161: NoneOrMore,
+                170: NoneOrOnce,
+                171: NoneOrMore,
+            },
+            owned_rights: {
+                1: NoneOrOnce,
+                160: NoneOrMore,
+                161: NoneOrMore,
+                170: NoneOrOnce,
+                171: NoneOrMore,
+            },
+            public_rights: {},
+            abi: {
+                Validate: Embedded(
+                    RightsSplit,
+                ),
+            },
+        },
+    },
+}"
+        );
+    }
+
+    #[test]
+    fn schema_strict_encode() {
+        let data = schema()
+            .strict_serialize()
+            .expect("RGB-20 schema serialization failed");
+
+        let bech32data = data.to_bech32data();
+        println!("{}", bech32data);
+
+        let schema20 = Schema::strict_deserialize(data)
+            .expect("RGB-20 schema deserialization failed");
+
+        assert_eq!(schema(), schema20);
+        assert_eq!(format!("{:#?}", schema()), format!("{:#?}", schema20));
+        assert_eq!(
+            bech32data,
+            "data1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq\
+        qqqqqqqqqqq2qqqqqpqgqqqsqpqqqypqqp8llupsqqqpqqfqgqqppqav0q2lqqqqqq8llll\
+        llllllal6qqqqpqqqqqqqqqqqqq8llllllllllllmqqqqpqqqqqqqqqqqqq8lllllllllll\
+        lmzqpqkgqqtlllkvqqxyqqqqqsyqcyq5rqwzqfpg9scrgwpuzsqqgqqqqqpgqqqgqqsqqqq\
+        qqqqqqqqrllllllllllllcqqzssqqgqpqqqqqqqqqqqqq8llllllllllllszqqqluq65qqq\
+        qqq2kqqqqqqqqqqxqqqqqqgqqyqqzqqpqqqsqqsqqqqqzqqrqqqsqqgqqsqqzqqpqzsqqqg\
+        qqyqqgqqpqqqqqqgq5qqqqq8ll7ssqqqqlll65qqqqqqsqqqqqqqqqqqqqqrsqqqqqqqqzq\
+        9pqqqsplllqyq2zqqqqrll7qqqqqqqqqqsqqzqqqqqqqqqzqqpqqqqqqgqqgqqqqqpqqpsq\
+        qqqqyqqzqqpqqqsqqgqqyqqzqqqqqqsqqqqqqqqqq9qqqqspgqqqyqqzqqpqzsqqqgqllls\
+        xq9qqqqqplll5yqqqq8ll74qqqqqqyqqqqqpqqq07qsqqzssqqqqqyq25qqpqqqsqqsq4gq\
+        qqqqpqz4sqqqqqyqqqqqqqqqqpgsqqsqtqqqpqqqspvgqqyq0llajqqqqplllkvqqzqqpqq\
+        qsp2cqqyqqzqqpqz4sqqqqqyqqqqqpqqq07yqqqz3sqpqqkqqqzqqpqzcsqqgqlllmyqqqq\
+        rlllvcqqyqqzqqpqz4sqqgqqyqqyq9pqqqsplll4vqqqqqpqqqqqqgqqrl3qqqq7qqqqqq9\
+        qqqsqqqqqyq2qqqqqrlllggqqqq0lla2qqqqqqgq4vqqqq8lluzsqqgqqqqqzq9qqqqqpll\
+        l5yqqqq8ll74qqqqqqyq2kqqqqrll7qqqqyqqpleqqqqqqqqz8ypqa"
+        );
+    }
+
+    #[test]
+    fn schema_bech32_encode() {
+        let data = schema().to_bech32_string();
+
+        println!("{}", data);
+
+        let schema20 = Schema::from_bech32_str(&data)
+            .expect("RGB-20 schema deserialization failed");
+
+        assert_eq!(schema(), schema20);
+        assert_eq!(format!("{:#?}", schema()), format!("{:#?}", schema20));
+        assert_eq!(
+            schema20.to_string(),
+            "schema1qxx4qkcjsgcqehyk7gg9lrp8uqw9a34r8r0qfay0lm\
+        cr3pxh7yrr2n2mvszq0s7symvkvdcf2ck6whm9zpgpqyk2nqypf8pget8vlk798ccuats4j\
+        zzn98ena4p2us7eyvmxvsz5zzvcc4yu5nvjdhlw76rkxn8vvs27f0qs4qyemfdfczyvve45\
+        qvfds8kryuuc4kzh03t2xruw932u6e7rn9szn8uz2kkcc7lrkzpw4ct4xpgej2s8e3vn224\
+        mmwh8yjwm3c3uzcsz350urqt6gfm6wpj6gcajd6uevncqy74u87jtfmx8raza9nlm2hazyd\
+        l7hyevmls6amyy4kl7rv6skggq"
+        );
+    }
+}
