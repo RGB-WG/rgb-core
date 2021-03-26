39ce113a0e7676d6e4692003de5f578484628d0f
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-03-26 21:10:22 +0100

    Finalizing and documenting stash API

--- src/stash/stash.rs
+++ src/stash/stash.rs
@@ -1,5 +1,5 @@
 // LNP/BP Rust Library
-// Written in 2020 by
+// Written in 2020-2021 by
 //     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
 //
 // To the extent possible under law, the author(s) have dedicated all
@@ -11,12 +11,29 @@
 // along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
-use lnpbp::seals::OutpointReveal;
+//! API for working with stash: storage of RGB contract client-side-validated
+//! data and data containers.
+//!
+//! Client-side-validated data, and (especially) data containers may grow large
+//! (multiple gigabytes) and can't be fit in a memory as a single data
+//! structure. Thus, we utilize a special API which abstracts the specific stash
+//! storage mechanism (file-based, SQL or NoSQL database, special disk
+//! partitions, cloud-provided storage, like with Bifrost protocol, etc).
+//! With this API the data can be accessed using iterators or by providing the
+//! specific data id.
+//!
+//! NB: Stash implementations must be able to operate multiple independent RGB
+//! contract.
+
 use std::collections::BTreeSet;
 
+use bitcoin::OutPoint;
+use lnpbp::seals::OutpointReveal;
+use wallet::resolvers::TxResolver;
+
 use crate::{
-    Anchor, Consignment, ContractId, Disclosure, Extension, Genesis, Node,
-    NodeId, SchemaId, SealEndpoint, Transition,
+    Anchor, AnchorId, Consignment, ContractId, Disclosure, Extension, Genesis,
+    Node, NodeId, Schema, SchemaId, SealEndpoint, Transition,
 };
 
 /// Top-level structure used by client wallets to manage all known RGB smart
@@ -25,43 +42,90 @@ use crate::{
 /// Stash operates blobs, so it does not keep in the memory whole copy of all
 /// data. Access to the underlying data structures happens through iterators:
 /// - [`Stash::ContractIterator`]
+///
+/// Stash API is an alternative to the RGB contract data access API provided by
+/// [`crate::GraphApi`], which is implemented by the structures keeping all the
+/// data in memory ([`Consignment`] and [`Disclosure`]).
 pub trait Stash {
     /// Error type returned by different stash functions
     type Error: std::error::Error;
 
+    /// Iterator implementation able to run over known schemata and subschemata
+    type SchemaIterator: Iterator<Item = Schema>;
+    /// Iterator implementation able to run over all contract geneses
     type GenesisIterator: Iterator<Item = Genesis>;
+    /// Iterator implementation able to run over all known anchors
     type AnchorIterator: Iterator<Item = Anchor>;
+    /// Iterator implementation able to run over all state transitions under
+    /// particular contract
     type TransitionIterator: Iterator<Item = Transition>;
+    /// Iterator implementation able to run over all state extensions under
+    /// particular contract
     type ExtensionIterator: Iterator<Item = Extension>;
-    type NidIterator: Iterator<Item = NodeId>;
+    /// Iterator implementation able to run over all node ids under
+    /// particular contract
+    type NodeIdIterator: Iterator<Item = NodeId>;
 
-    fn get_schema(&self, schema_id: SchemaId) -> Result<SchemaId, Self::Error>;
+    /// Returns schema or subschema matching the provided id, if any, or
+    /// storage-specific error otherwise
+    fn get_schema(&self, schema_id: SchemaId) -> Result<Schema, Self::Error>;
 
+    /// Returns genesis matching the provided id, if any, or storage-specific
+    /// error otherwise
     fn get_genesis(
         &self,
         contract_id: ContractId,
     ) -> Result<Genesis, Self::Error>;
 
+    /// Returns state transition matching the provided `node_id`, if any, or
+    /// storage-specific error otherwise.
+    ///
+    /// NB: Here the state transition is identified by the node id and not
+    /// relates to a specific contract_id. To get the transitions by a contract
+    /// id please use transition iterator.
     fn get_transition(
         &self,
         node_id: NodeId,
     ) -> Result<Transition, Self::Error>;
 
+    /// Returns state extension matching the provided `node_id`, if any, or
+    /// storage-specific error otherwise.
+    ///
+    /// NB: Here the state extension is identified by the node id and not
+    /// relates to a specific contract_id. To get the extensions by a contract
+    /// id please use extension iterator.
     fn get_extension(&self, node_id: NodeId) -> Result<Extension, Self::Error>;
 
-    fn get_anchor(&self, anchor_id: ContractId) -> Result<Anchor, Self::Error>;
+    /// Returns anchor matching the provided `anchor_id`, if any, or
+    /// storage-specific error otherwise.
+    ///
+    /// NB: Anchors may be related to multiple contract ids; specific set of the
+    /// contracts to which this anchor is related to may be known from the
+    /// anchor data, unless they are kept in the confidential form. See
+    /// [`Anchor`] documentation for the details.
+    fn get_anchor(&self, anchor_id: AnchorId) -> Result<Anchor, Self::Error>;
 
-    /// A contract is a genesis
+    /// Iterator over all contract geneses (i.e. iterator over all known RGB
+    /// contracts).
     fn genesis_iter(&self) -> Self::GenesisIterator;
 
-    /// We have to keep anchors at this level, since they may link many
-    /// state transitions under multiple contracts at the same time (via
-    /// LNPBP-4 multimessage commitments)
+    /// Iterator over all known anchors
+    ///
+    /// NB: each anchor may be related to multiple contracts, thus here we do
+    /// not provide contract id constraint for the iterator.
     fn anchor_iter(&self) -> Self::AnchorIterator;
 
-    fn transition_iter(&self) -> Self::TransitionIterator;
+    /// Iterator over all known state transition under particular RGB contract
+    fn transition_iter(
+        &self,
+        contract_id: ContractId,
+    ) -> Self::TransitionIterator;
 
-    fn extension_iter(&self) -> Self::ExtensionIterator;
+    /// Iterator over all known state extensions under particular RGB contract
+    fn extension_iter(
+        &self,
+        contract_id: ContractId,
+    ) -> Self::ExtensionIterator;
 
     /// When we need to send over to somebody else an update (like we have
     /// transferred him some state, for instance an asset) for each transfer we
@@ -86,19 +150,18 @@ pub trait Stash {
         known_seals: &Vec<OutpointReveal>,
     ) -> Result<(), Self::Error>;
 
-    /// Acquire knowledge from a given disclosure
-    fn know_about(&mut self, disclosure: Disclosure)
-        -> Result<(), Self::Error>;
-
-    /// If we need to forget about the state which is not owned by us anymore
-    /// (we have done the transfer and would like to prune this specific info)
-    /// we call this function
-    fn forget(
-        &mut self,
-        consignment: Consignment,
-    ) -> Result<usize, Self::Error>;
+    /// Acquire knowledge from a given disclosure (**enclose** procedure)
+    fn enclose(&mut self, disclosure: &Disclosure) -> Result<(), Self::Error>;
 
     /// Clears all data that are not related to the contract state owned by
-    /// us in this moment — under all known contracts
-    fn prune(&mut self) -> Result<usize, Self::Error>;
+    /// us in this moment — under all known contracts. Uses provided
+    /// `tx_resolver` to resolve validity of the related transactions (witness
+    /// and single-use-seal) and `ownership_resolver` for checking whether
+    /// specific transaction output is owned by the current user (stash data
+    /// holder)
+    fn prune(
+        &mut self,
+        tx_resolver: &mut impl TxResolver,
+        ownership_resolver: impl Fn(OutPoint) -> bool,
+    ) -> Result<usize, Self::Error>;
 }
