dcf8e91e2e8ae11715e5e72b747299134ee4a16a
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2021-03-26 21:12:37 +0100

    Storage/data container interator API

--- src/stash/iter.rs
+++ src/stash/iter.rs
@@ -14,47 +14,109 @@
 //! Iterators for stash-related data structures ([`Stash`] implementations,
 //! [`Consignment`]s, [`Disclosure`]s)
 
-use std::slice;
-
 use bitcoin::Txid;
 
-use crate::{Anchor, Consignment, Transition};
+use crate::schema::OwnedRightType;
+use crate::{
+    Consignment, ConsistencyError, GraphApi, Node, NodeId, Transition,
+};
 
+/// Generic trait for iterators over state transitions contained in
+/// client-side-validation containers
 pub trait TransitionIterator<'iter> {
-    type Iterator: Iterator<Item = (Txid, &'iter Transition)>;
+    /// Concrete type implementing the iterator
+    type Iterator: Iterator<Item = (&'iter Transition, Txid)>;
 }
 
 impl<'iter> TransitionIterator<'iter> for Consignment {
-    type Iterator = ConsTsIter<'iter>;
+    type Iterator = CsChainIter<'iter>;
 }
 
 /// Iterator over transitions and corresponding witness transaction ids which
-/// can be created out of consignment data
+/// can be created out of consignment data. Transitions of this type must be
+/// organized into a chain connecting 1-to-1 via the provided `connected_by`
+/// during iterator creation.
+///
+/// Iterator is created with [`Consignment::chain_iter`]
 #[derive(Debug)]
-pub struct ConsTsIter<'iter> {
+pub struct CsChainIter<'iter> {
     consignment: &'iter Consignment,
-    iter: slice::Iter<'iter, (Anchor, Transition)>,
+    connected_by: OwnedRightType,
+    next_item: Option<(&'iter Transition, Txid)>,
+    error: Option<ConsistencyError>,
 }
 
-impl Consignment {
-    /// Creates iterator over all state transitions present in the consignment
-    /// and corresponding witness transaction ids
-    pub fn transition_iter(&self) -> ConsTsIter {
-        ConsTsIter {
-            consignment: self,
-            iter: self.state_transitions.iter(),
+impl<'iter> CsChainIter<'iter> {
+    /// Detects whether iterator was stopped by a error
+    pub fn is_err(&'iter self) -> bool {
+        self.error.is_some()
+    }
+
+    /// Converts iterator into a result type prividing information about the
+    /// error (if any) which terminated execution of the iterator
+    pub fn into_result(self) -> Result<(), ConsistencyError> {
+        if let Some(err) = self.error {
+            Err(err)
+        } else {
+            Ok(())
         }
     }
 }
 
-impl<'iter> Iterator for ConsTsIter<'iter> {
-    type Item = (Txid, &'iter Transition);
+impl Consignment {
+    /// Creates iterator over a single chain of state transition starting from
+    /// `node_id` which must be one of the consignment endpoints, and
+    /// corresponding witness transaction ids. Transitions must be organized
+    /// into a chain connecting 1-to-1 via the provided `connected_by` owned
+    /// rights (one or none of them must be present for each state transition).
+    pub fn chain_iter(
+        &self,
+        start_with: NodeId,
+        connected_by: OwnedRightType,
+    ) -> CsChainIter {
+        let next_item = self
+            .endpoint_transition_by_id(start_with)
+            .into_iter()
+            .next()
+            .and_then(|_| self.transition_witness_by_id(start_with).ok());
+
+        CsChainIter {
+            consignment: self,
+            connected_by,
+            next_item,
+            error: None,
+        }
+    }
+}
+
+impl<'iter> Iterator for CsChainIter<'iter> {
+    type Item = (&'iter Transition, Txid);
 
     fn next(&mut self) -> Option<Self::Item> {
-        if let Some((anchor, transition)) = self.iter.next() {
-            Some((anchor.txid, transition))
+        let item = if let Some(item) = self.next_item {
+            item
         } else {
             return None;
-        }
+        };
+
+        let output = if let Some(output) = item
+            .0
+            .parent_outputs_by_type(self.connected_by)
+            .first()
+            .copied()
+        {
+            output
+        } else {
+            self.next_item = None;
+            return None;
+        };
+
+        self.next_item = self
+            .consignment
+            .transition_witness_by_id(output.node_id)
+            .map_err(|err| self.error = Some(err))
+            .ok();
+
+        Some(item)
     }
 }
