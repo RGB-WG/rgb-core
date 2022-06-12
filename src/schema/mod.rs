d067a233c5eb707402ccb3731148ae8f76573bb9
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-12 10:48:50 +0200

    Remove unused DataFormat type
    After switching to strict encoding type system this type is not used anymore

--- src/schema/mod.rs
+++ src/schema/mod.rs
@@ -26,7 +26,7 @@ pub use script::{
     Action, AssignmentAbi, AssignmentAction, ExecutableCode, ExtensionAbi, ExtensionAction,
     GenericAction, GenesisAbi, GenesisAction, NodeAction, TransitionAbi, TransitionAction, VmType,
 };
-pub use state::{DataFormat, DiscreteFiniteFieldFormat, StateFormat, StateSchema, StateType};
+pub use state::{DiscreteFiniteFieldFormat, StateFormat, StateSchema, StateType};
 pub use types::{Bits, Occurrences, OccurrencesError};
 
 mod verify {
