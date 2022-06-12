fb4ac016ae081b5c72674a3d783ee9c53f03fa25
Dr Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-06-12 14:28:21 +0200

    Move schema constants to embedded VM

--- src/vm/embedded.rs
+++ src/vm/embedded.rs
@@ -18,11 +18,138 @@ use bitcoin::OutPoint;
 use commit_verify::CommitConceal;
 
 use super::VmApi;
-use crate::schema::constants::*;
 use crate::{
     schema, validation, value, AssignmentVec, Metadata, NodeId, NodeOutput, NodeSubtype,
     OwnedRights, PublicRights, Transition,
 };
+
+/// Constants which are common to different schemata and can be recognized
+/// by the software even if the specific schema is unknown, since this type ids
+/// are reserved to a specific semantic meaning
+///
+/// These constants are also used by embedded validation procedures representing
+/// standard metadata fields, state and transition types analyzed by them
+mod constants {
+    /// Ticker of the asset
+    pub const FIELD_TYPE_TICKER: u16 = 0x00;
+
+    /// Contract or asset name
+    pub const FIELD_TYPE_NAME: u16 = 0x01;
+
+    /// Ricardian contract text
+    pub const FIELD_TYPE_CONTRACT_TEXT: u16 = 0x02;
+
+    /// Decimal precision for some sort of amount values used in a contract
+    pub const FIELD_TYPE_PRECISION: u16 = 0x03;
+
+    /// Timestamp used in genesis to indicate moment of contract creation
+    pub const FIELD_TYPE_TIMESTAMP: u16 = 0x04;
+
+    /// Generic comment about the contract or a state transitions
+    pub const FIELD_TYPE_COMMENTARY: u16 = 0x05;
+
+    /// Data attached to a state transition in binary format
+    pub const FIELD_TYPE_DATA: u16 = 0x10;
+
+    /// Format of the attached data, schema-specific
+    // TODO #36: Use LNPBP-extended MIME types embedded to data containers
+    pub const FIELD_TYPE_DATA_FORMAT: u16 = 0x11;
+
+    /// [`FieldType`] that is used by validation procedures checking the issued
+    /// supply & inflation
+    pub const FIELD_TYPE_ISSUED_SUPPLY: u16 = 0xA0;
+
+    /// [`FieldType`] that is used by validation procedures checking proofs of
+    /// burn. Must contain amount of the burned supply, expressed as
+    /// revealed [`crate::value::Revealed`] data
+    pub const FIELD_TYPE_BURN_SUPPLY: u16 = 0xB0;
+
+    /// [`FieldType`] that is used by validation procedures checking proofs of
+    /// burn. Must contain [`bitcoin::OutPoint`] consensus-encoded data.
+    pub const FIELD_TYPE_BURN_UTXO: u16 = 0xB1;
+
+    /// [`FieldType`] that is used by validation procedures checking proofs of
+    /// burn. Must contain binary data ([`crate::data::Revealed::Bytes`])
+    pub const FIELD_TYPE_HISTORY_PROOF: u16 = 0xB2;
+
+    /// [`FieldType`] that is used by validation procedures checking proofs of
+    /// burn. Must contain format of the provided proofs defined in
+    /// [`HistoryProofFormat`]
+    pub const FIELD_TYPE_HISTORY_PROOF_FORMAT: u16 = 0xB3;
+
+    /// [`FieldType`] that is used by validation procedures checking proofs of
+    /// reserves. Must contain [`wallet::descriptor::Expanded`] strict encoded
+    /// data.
+    pub const FIELD_TYPE_LOCK_DESCRIPTOR: u16 = 0xC0;
+
+    /// [`FieldType`] that is used by validation procedures checking proofs of
+    /// reserves. Must contain [`bitcoin::OutPoint`] consensus-encoded data
+    pub const FIELD_TYPE_LOCK_UTXO: u16 = 0xC1;
+
+    // --------
+
+    /// Renomination of the contract parameters
+    pub const STATE_TYPE_RENOMINATION_RIGHT: u16 = 0x01;
+
+    /// [`OwnedRightType`] that is used by the validation procedures checking
+    /// asset inflation (applies to both fungible and non-fungible assets)
+    pub const STATE_TYPE_INFLATION_RIGHT: u16 = 0xA0;
+
+    /// [`OwnedRightType`] that is used by validation procedures checking for
+    /// the equivalence relations between previous and new asset ownership
+    /// (applies to both fungible and non-fungible assets).
+    ///
+    /// NB: STATE_TYPE_OWNERSHIP_RIGHT + N where N = 1..9 are reserved for
+    ///     custom forms of ownership (like "engraved ownership" for NFT tokens)
+    pub const STATE_TYPE_OWNERSHIP_RIGHT: u16 = 0xA1;
+
+    /// Right to define epochs of asset replacement
+    pub const STATE_TYPE_ISSUE_EPOCH_RIGHT: u16 = 0xAA;
+
+    /// Right to replace some of the state issued under the contract
+    pub const STATE_TYPE_ISSUE_REPLACEMENT_RIGHT: u16 = 0xAB;
+
+    /// Right to replace some of the state issued under the contract
+    pub const STATE_TYPE_ISSUE_REVOCATION_RIGHT: u16 = 0xAC;
+
+    // --------
+
+    /// Transitions transferring ownership over primary contract state
+    pub const TRANSITION_TYPE_OWNERSHIP_TRANSFER: u16 = 0x00;
+
+    /// Transitions modifying primary contract state, possibly combining with
+    /// ownership transfer
+    pub const TRANSITION_TYPE_STATE_MODIFICATION: u16 = 0x01;
+
+    /// Transition performing renomination of contract metadata
+    pub const TRANSITION_TYPE_RENOMINATION: u16 = 0x10;
+
+    /// [`TransitionType`] that is used by the validation procedures checking
+    /// asset inflation (applies to both fungible and non-fungible assets)
+    pub const TRANSITION_TYPE_ISSUE: u16 = 0xA0;
+
+    /// Transition that defines certain grouping of other issue-related
+    /// operations
+    pub const TRANSITION_TYPE_ISSUE_EPOCH: u16 = 0xA1;
+
+    /// Transition burning some of the issued contract state.
+    ///
+    /// NB: It is not the same as [`TRANSITION_TYPE_RIGHTS_TERMINATION`], which
+    /// terminates ability to utilize some rights (but not the state)
+    pub const TRANSITION_TYPE_ISSUE_BURN: u16 = 0xA2;
+
+    /// Transition replacing some of the previously issued state with the new
+    /// one
+    pub const TRANSITION_TYPE_ISSUE_REPLACE: u16 = 0xA3;
+
+    /// Transition performing split of rights assigned to the same UTXO by
+    /// a mistake
+    pub const TRANSITION_TYPE_RIGHTS_SPLIT: u16 = 0xF0;
+
+    /// Transition making certain rights void without executing the right itself
+    pub const TRANSITION_TYPE_RIGHTS_TERMINATION: u16 = 0xFF;
+}
+use constants::*;
 
 /// Trait for all embedded handlers which allows their construction from
 /// entry point data (which must represent the id of the embedded procedure)
@@ -296,6 +423,7 @@ pub enum HandlerError {
     DataEncoding,
 }
 
+// TODO: Refactor node validator
 impl NodeValidator {
     pub(self) fn validate(
         &self,
@@ -413,17 +541,8 @@ impl NodeValidator {
         Ok(())
     }
 
-    fn proof_of_burn(meta: &Metadata) -> Result<(), HandlerError> {
-        let format = meta
-            .u8(FIELD_TYPE_HISTORY_PROOF_FORMAT)
-            .first()
-            .copied()
-            .ok_or(HandlerError::BrokenSchema)?;
-
-        match schema::HistoryProofFormat::from_u8(format).ok_or(HandlerError::BrokenSchema)? {
-            schema::HistoryProofFormat::ProofAbsent => Ok(()),
-            _ => Err(HandlerError::NotImplemented),
-        }
+    fn proof_of_burn(_meta: &Metadata) -> Result<(), HandlerError> {
+        Err(HandlerError::NotImplemented)
     }
 
     fn proof_of_reserve(meta: &Metadata) -> Result<(), HandlerError> {
