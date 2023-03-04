245c2f1ec6c7b199c558ae963c4fbe7aef52c87d
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-03-04 16:45:16 +0100

    vm: add basics for RGB ISAE

--- src/vm/isa.rs
+++ src/vm/isa.rs
@@ -29,14 +29,44 @@ use aluvm::isa::{Bytecode, BytecodeError, ExecStep, InstructionSet};
 use aluvm::program::{CodeEofError, LibSite, Read, Write};
 use aluvm::reg::CoreRegs;
 
-pub const INSTR_NOOP: u8 = 0b11_000_000;
-pub const INSTR_ISAE_FROM: u8 = 0b11_000_000;
-pub const INSTR_ISAE_TO: u8 = 0b11_000_000;
+pub const INSTR_PCVS: u8 = 0b11_001_000;
+// NB: For now we prohibit all other ISAE than this one. More ISAEs can be
+// allowed in a future with fast-forwards.
+pub use aluvm::isa::opcodes::{INSTR_ISAE_FROM, INSTR_ISAE_TO};
+// pub const INSTR_ISAE_FROM: u8 = 0b11_000_000;
+// pub const INSTR_ISAE_TO: u8 = 0b11_000_000;
 
 #[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug, Display)]
 pub enum RgbIsa {
-    #[display("noop")]
-    Noop,
+    /// Verify sum of pedersen commitments from inputs and outputs.
+    ///
+    /// The only argument specifies owned state type for the sum operation. If
+    /// this state does not exists, either inputs or outputs does not have
+    /// any data for the state, or the state is not
+    /// of `FungibleState::Bits64` fails the verification.
+    ///
+    /// If verification succeeds, doesn't changes `st0` value; otherwise sets it
+    /// to `false`.
+    #[display("pcvs     {0}")]
+    PcVs(u16),
+
+    /*
+    /// Verifies corrected sum of pedersen commitments adding a value taken from `RegR` to the list
+    /// of inputs (negatives).
+    PcCs(u16, RegR),
+
+    /// Loads owned state into a register.
+    LdOs(),
+
+    /// Loads global state item into a register.
+    LdGs(),
+
+    /// Loads operation info into a string register.
+    LdOp(),
+     */
+    /// All other future unsupported operations, which must set `st0` to
+    /// `false`.
+    Fail(u8),
 }
 
 impl InstructionSet for RgbIsa {
@@ -52,7 +82,8 @@ impl InstructionSet for RgbIsa {
 impl Bytecode for RgbIsa {
     fn byte_count(&self) -> u16 {
         match self {
-            RgbIsa::Noop => 0,
+            RgbIsa::PcVs(_) => 2,
+            RgbIsa::Fail(_) => 0,
         }
     }
 
@@ -60,14 +91,16 @@ impl Bytecode for RgbIsa {
 
     fn instr_byte(&self) -> u8 {
         match self {
-            Self::Noop => INSTR_NOOP,
+            Self::PcVs(_) => INSTR_PCVS,
+            RgbIsa::Fail(other) => *other,
         }
     }
 
-    fn encode_args<W>(&self, _writer: &mut W) -> Result<(), BytecodeError>
+    fn encode_args<W>(&self, writer: &mut W) -> Result<(), BytecodeError>
     where W: Write {
         match self {
-            RgbIsa::Noop => {}
+            RgbIsa::PcVs(state_type) => writer.write_u16(*state_type)?,
+            RgbIsa::Fail(_) => {}
         }
         Ok(())
     }
@@ -78,8 +111,8 @@ impl Bytecode for RgbIsa {
         R: Read,
     {
         Ok(match reader.read_u8()? {
-            INSTR_NOOP => Self::Noop,
-            _ => Self::Noop,
+            INSTR_PCVS => Self::PcVs(reader.read_u16()?),
+            other => Self::Fail(other),
         })
     }
 }
