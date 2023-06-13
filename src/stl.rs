57d1b2b63dde2f386ccf2f33a09c45f3eaa59b1a
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-06-13 19:04:33 +0200

    stl: fix use of deprecated error type

--- src/stl.rs
+++ src/stl.rs
@@ -24,15 +24,15 @@ use aluvm::stl::aluvm_stl;
 use bp::bc::stl::bitcoin_stl;
 use bp::stl::bp_core_stl;
 use strict_types::stl::strict_types_stl;
-use strict_types::typelib::{LibBuilder, TranslateError};
-use strict_types::TypeLib;
+use strict_types::typelib::LibBuilder;
+use strict_types::{CompileError, TypeLib};
 
 use crate::{Extension, Genesis, SubSchema, TransitionBundle, LIB_NAME_RGB};
 
 /// Strict types id for the library providing data types for RGB consensus.
 pub const LIB_ID_RGB: &str = "fiber_deal_falcon_3NhgEBNcHTwSGZ1zeoGvebAAZrkhxio9HEkc7dYUjaAF";
 
-fn _rgb_core_stl() -> Result<TypeLib, TranslateError> {
+fn _rgb_core_stl() -> Result<TypeLib, CompileError> {
     LibBuilder::new(libname!(LIB_NAME_RGB), tiny_bset! {
         strict_types_stl().to_dependency(),
         bitcoin_stl().to_dependency(),
