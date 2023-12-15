4aba65f969cfaa821e5d29297e2b5d596ba22982
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2023-12-15 17:43:19 +0100

    contract: remove AnchorId type

--- src/stl.rs
+++ src/stl.rs
@@ -32,7 +32,7 @@ use crate::{AnchoredBundle, ContractState, Extension, Genesis, SubSchema, LIB_NA
 
 /// Strict types id for the library providing data types for RGB consensus.
 pub const LIB_ID_RGB: &str =
-    "urn:ubideco:stl:Cx3Lxt83gdcuXnTxTvSXfxyZUeNi5fqaiibEYr6o7jxy#panic-genius-delphi";
+    "urn:ubideco:stl:CjHnUSMpAUVxemMGtj8hJtcCssguGxDghjBzfLrUfiMW#action-ribbon-diana";
 
 fn _rgb_core_stl() -> Result<TypeLib, CompileError> {
     LibBuilder::new(libname!(LIB_NAME_RGB), tiny_bset! {
