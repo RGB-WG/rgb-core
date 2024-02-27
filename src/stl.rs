bcd78e4865465abee92a8dc435e5608b5faa3e4d
Dr Maxim Orlovsky <orlovsky@lnp-bp.org>
2024-02-27 21:21:21 +0100

    stl: update

--- src/stl.rs
+++ src/stl.rs
@@ -34,7 +34,7 @@ use crate::{
 
 /// Strict types id for the library providing data types for RGB consensus.
 pub const LIB_ID_RGB: &str =
-    "urn:ubideco:stl:HqdNNAehK3yaQ1AC5pQmEuNNS1zgyo2Jw31fdUNKb9MU#annual-virus-garlic";
+    "urn:ubideco:stl:AdsYXPsV27aqRCBxrrMs55FPZRAMxhEjyezr5ULGNY8J#gemini-plato-license";
 
 fn _rgb_core_stl() -> Result<TypeLib, CompileError> {
     LibBuilder::new(libname!(LIB_NAME_RGB), tiny_bset! {
