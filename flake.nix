b94bc06352e9cbab095296e4d64c5fd49ee35c24
Antonio Yang <yanganto@gmail.com>
2024-04-08 11:37:43 +0800

    ci: remove actions-rs use nix to check

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, rust-overlay, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      in
+      with pkgs;
+      {
+        devShells = rec {
+          default = msrv;
+
+          msrv = mkShell {
+            buildInputs = [
+              rust-bin.stable."${cargoToml.package."rust-version"}".default
+            ];
+          };
+
+          stable = mkShell {
+            buildInputs = [
+              rust-bin.stable.latest.default
+            ];
+          };
+
+          beta = mkShell {
+            buildInputs = [
+              rust-bin.beta.latest.default
+            ];
+          };
+
+          nightly = mkShell {
+            buildInputs = [
+              rust-bin.nightly.latest.default
+            ];
+          };
+        };
+      }
+    );
+}
